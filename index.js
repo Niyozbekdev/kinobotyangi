03ad8c266d9cd1bac071a0bd0335669890031e66
Niyozbek <niyozbekallaberdiyev@gmail.com>
2025-05-27 01:01:43 +0500

    Env fayl o'chirildi

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const connectDB = require('./utils/connectDB');
 const bot = require('./bot');
 
 (async () => {
-    try {
-        await connectDB();   // Mongo bazaga ulanadi
-        bot.launch();        // Bot endi ishga tushadi
-    } catch (err) {
-        console.error("Index.js consol", err)
-    }
+
+    await connectDB();   // Mongo bazaga ulanadi
+    bot.launch();        // Bot endi ishga tushadi
+
 })()
