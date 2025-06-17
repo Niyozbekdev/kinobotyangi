68ba4496f3df40127af793f6449f6d6d3841b774
Niyozbek <niyozbekallaberdiyev@gmail.com>
2025-06-17 23:30:05 +0500

    http qushildi bot uxlab qolmasligi uchun

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
+const http = require('http');
 const connectDB = require('./utils/connectDB');
 const bot = require('./bot');
 
 (async () => {
 
     await connectDB();   // Mongo bazaga ulanadi
-    //Webhookni tozalash
-    bot.telegram.deleteWebhook().then(() => {
-        bot.launch();       // Bot endi ishga tushadi polling rejmida
-    })
+    bot.launch();       // Bot endi ishga tushadi polling rejmida
+
+    http.createServer((req, res) => {
+        res.writeHead(200)
+    }).listen(process.env.Port || 3001);
 
 })();
